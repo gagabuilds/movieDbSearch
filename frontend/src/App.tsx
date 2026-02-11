e74bc98f4d552c63f2f97577029afe547fe361fa
abderrahman Gaga <abderrahman@gagabuilds.com>
2026-02-11 17:45:23 +0200

    Fix: bind nestjs to 0.0.0.0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [healthStatus, setHealthstatus] = useState('checkiing...')
+
+  useEffect(() => {
+    fetch('http://localhost:3000/search?q=titina')
+    .then(response => response.ok ? setHealthstatus('Healthy') : setHealthstatus('Unhealthy'))
+    .catch(() => setHealthstatus('Error'))
+  }, [])
 
   return (
     <>
@@ -28,6 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>API Health: {healthStatus}</p>
     </>
   )
 }
