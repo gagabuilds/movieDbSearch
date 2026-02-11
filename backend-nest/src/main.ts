5f3a20fe527cb45635f8ac8e6a8bb807957f8074
abderrahman Gaga <abderrahman@gagabuilds.com>
2026-02-11 22:29:11 +0200

    UPDATE: action workflow - smoke test using httpie

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -3,7 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  app.enableCors({
+    origin: true,
+    Credentials: false,
+  })
+  
+  await app.listen(3000, '0.0.0.0');
+  
+  
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [healthStatus, setHealthstatus] = useState('checkiing...')
+  const [healthStatus, setHealthstatus] = useState('checking...')
 
   useEffect(() => {
-    fetch('http://localhost:3000/search?q=titina')
+    const apiUrl = import.meta.env.VITE_API_URL;
+    fetch(`${apiUrl}/health`)
     .then(response => response.ok ? setHealthstatus('Healthy') : setHealthstatus('Unhealthy'))
     .catch(() => setHealthstatus('Error'))
   }, [])
@@ -16,26 +14,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Transcendence</h1>
       </div>
-      <h1>Transcendence</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>API Health: {healthStatus}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>API Health: {healthStatus}</p>
     </>
   )
 }
