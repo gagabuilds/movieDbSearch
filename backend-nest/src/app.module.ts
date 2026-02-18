cfa34343ffde4a52185f7f679c8ab7f892de51c9
abderrahman Gaga <abderrahman@gagabuilds.com>
2026-02-18 18:12:44 +0200

    feat(user): implement user management module

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -5,13 +5,15 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, }),
     HttpModule,
     PrismaModule,
-    AuthModule
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
