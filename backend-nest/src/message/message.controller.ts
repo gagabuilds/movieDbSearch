2655ef15af8c21a85b53c460249964e5decf50f0
Emilia Rantalainen <erantala@hive.student.fi>
2026-04-01 14:33:37 +0300

    removed unnecessary auth guard

--- backend-nest/src/message/message.controller.ts
+++ backend-nest/src/message/message.controller.ts
@@ -8,8 +8,6 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 export class MessageController {
 	constructor(private readonly messageService: MessageService) {}
 
-
-	@UseGuards(JwtAuthGuard)
 	@Post('rooms/create/:friendId')
 	async getCreateRoom(@Request() req, @Param('friendId') userId2: string)
 	{
@@ -20,7 +18,6 @@ export class MessageController {
 		return room;
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Post('rooms/:roomId/chat')
 	async sendMessage( @Request() Req, @Param('roomId') roomId: string, @Body() dto: CreateMessageDto,)
 	{
@@ -28,26 +25,22 @@ export class MessageController {
 		return (message);
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Get('rooms/:userId')
 	async getUserRooms(@Request() req) {
 		return await this.messageService.getChatRooms(req.user.id)
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Get('menu/rooms')
 	async getMenuRooms(@Request() req): Promise<MenuRoom[]> {
   		return this.messageService.getMenuRooms(req.user.id);
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Get('rooms/:roomId/messages')
 	async getRoomMessages(@Request() Req, @Param('roomId') roomId: string)
 	{
 		return await this.messageService.getRoomMessages(roomId);
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Get('/rooms/:roomId/info')
 	async getRoomInfo(@Request() Req, @Param('roomId') roomId: string)
 	{
