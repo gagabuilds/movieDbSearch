e7c6ea879d091f8ff8df1bbcc3690be528d8fb7a
ThiwankaS <thivankas@outlook.com>
2026-04-04 02:38:14 +0300

    add comments to improve read-ability and update friends count API to match REST API std

--- backend-nest/src/friends/friends.controller.ts
+++ backend-nest/src/friends/friends.controller.ts
@@ -4,7 +4,7 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('friends')
 export class FriendsController {
-  constructor(private readonly friendsService: FriendsService) {}
+  constructor(private readonly friendsService: FriendsService) { }
 
   @UseGuards(JwtAuthGuard)
   @Post(':id')
@@ -24,7 +24,7 @@ export class FriendsController {
     return this.friendsService.getFriends(req.user.id);
   }
 
-  @Get(':id')
+  @Get(':id/count')
   getFriendscount(@Request() req, @Param('id') userId: string) {
     return this.friendsService.getFriendsCount(userId);
   }
