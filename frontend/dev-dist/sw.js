35d0462804fbf91b814a5ea6008f78cddf527a65
Erantala <erantala@student.hive.fi>
2026-04-06 13:49:00 +0300

    updated docker

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ol4kruo1a98"
+    "revision": "0.0bjljm5hsrk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
