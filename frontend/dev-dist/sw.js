dc79af2b23502dc1271e3674bbb81205ebe998b1
ThiwankaS <thivankas@outlook.com>
2026-04-10 04:34:22 +0300

    resoving commit conflitcs

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.14nhu8f05c4"
+    "revision": "0.maive7g4d1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
